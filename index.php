a033b4d97e3eb04e19886369998fed0ebc88c756
Skip Keats <wilfredk@ekuber.com>
2018-09-14 14:20:46 -0400

    cpvg-0.0.7.20.2.3: Working through cpvg post view code fix step-by-step.

--- index.php
+++ index.php
@@ -357,7 +357,9 @@ function cpvg_post_views() {
 
 	$post_types = array_diff_assoc(
 		get_post_types(
-			array('_builtin' => false), 'names'), array('content-type' => 'content-type', 'rw_content_type' => 'rw_content_type', 'rw_taxonomy' => 'rw_taxonomy'));
+			array(
+				'_builtin' => false,
+			), 'names'), array('content-type' => 'content-type', 'rw_content_type' => 'rw_content_type', 'rw_taxonomy' => 'rw_taxonomy'));
 	?>
 	<div id='cpvg-wrap' class='wrap cpvg-post-views'>
 		<div id='icon-edit-pages' class='icon32'><br></div>
